d252314b9d62d43b41016e1c45d54ab839a32ceb
mramos <mramos@MRAMOS-LT-01.tn.virtela.cc>
2017-09-04 05:08:46 +0800

    Added JSTL test

--- src/main/java/ph/mramos/ocjsfd/controller/TagController.java
+++ src/main/java/ph/mramos/ocjsfd/controller/TagController.java
@@ -1,7 +1,9 @@
 package ph.mramos.ocjsfd.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
-import javax.faces.component.UIForm;
 import javax.faces.component.UIOutput;
 import javax.faces.component.html.HtmlForm;
 import javax.faces.event.ActionEvent;
@@ -17,10 +19,7 @@ public class TagController {
 	private static final Logger log = LoggerFactory.getLogger(TagController.class);
 	
 	private String creditCard = "1234 1234 1234 1234";
-
-	public String getCreditCard() {
-		return creditCard;
-	}
+	private List<Integer> numbers = Arrays.asList(5, 6, 7, 8, 9);
 	
 	public void form2ActionListener(ActionEvent event) {
 		UIOutput _out0 = (UIOutput) event.getComponent().findComponent(":out0");
@@ -38,6 +37,14 @@ public class TagController {
 		log.info("out2 = " + out2.getValue());
 		log.info(":form1 = " + _form1.getLang());
 		log.info("form2 = " + form2.getLang());
+	}
+
+	public String getCreditCard() {
+		return creditCard;
+	}
+	
+	public List<Integer> getNumbers() {
+		return numbers;
 	}
 	
 }
