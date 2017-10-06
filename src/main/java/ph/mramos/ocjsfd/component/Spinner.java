2e3bf0725438ee3c6b6f5dd30ce2aa9620c8b52b
mramos <mramos@MRAMOS-DT-02.tn.virtela.cc>
2017-10-06 19:35:58 +0800

    Added external renderer test

---
+++ src/main/java/ph/mramos/ocjsfd/component/Spinner.java
@@ -0,0 +1,20 @@
+package ph.mramos.ocjsfd.component;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIInput;
+import javax.faces.convert.IntegerConverter;
+
+@FacesComponent("ph.mramos.ocjsfd.component.Spinner")
+public class Spinner extends UIInput {
+
+	public Spinner() {
+		setConverter(new IntegerConverter());
+//		setRendererType("ph.mramos.ocjsfd.component.SpinnerRenderer");
+	}
+
+	@Override
+	public String getFamily() {
+		return "ph.mramos.ocjsfd.component";
+	}
+
+}
--- src/main/java/ph/mramos/ocjsfd/component/SpinnerRenderer.java
+++ src/main/java/ph/mramos/ocjsfd/component/SpinnerRenderer.java
@@ -3,60 +3,59 @@ package ph.mramos.ocjsfd.component;
 import java.io.IOException;
 import java.util.Map;
 
-import javax.faces.component.FacesComponent;
-import javax.faces.component.UIInput;
+import javax.faces.component.EditableValueHolder;
+import javax.faces.component.UIComponent;
+import javax.faces.component.ValueHolder;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
-import javax.faces.convert.IntegerConverter;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.render.FacesRenderer;
+import javax.faces.render.Renderer;
 
-@FacesComponent("ph.mramos.ocjsfd.Spinner")
-public class UISpinner extends UIInput {
+@FacesRenderer(componentFamily = "ph.mramos.ocjsfd.component", rendererType = "ph.mramos.ocjsfd.component.SpinnerRenderer")
+public class SpinnerRenderer extends Renderer {
 
-	public UISpinner() {
-		setConverter(new IntegerConverter());
-		setRendererType(null);
-	}
-	
 	@Override
-	public void encodeBegin(FacesContext fc) throws IOException {
+	public void encodeBegin(FacesContext fc, UIComponent component) throws IOException {
 		ResponseWriter writer = fc.getResponseWriter();
-		String clientId = getClientId(fc);
-		
+		String clientId = component.getClientId(fc);
+
 		// input field
-		writer.startElement("input", this);
+		writer.startElement("input", component);
 		writer.writeAttribute("name", clientId, null);
-		Object value = getValue();
+		Object value = ((ValueHolder) component).getValue();
 		if (value != null) {
 			writer.writeAttribute("value", value, "value");
 		}
-		Object size = getAttributes().get("size");
+		Object size = component.getAttributes().get("size");
 		if (size != null) {
 			writer.writeAttribute("size", size, "size");
 		}
 		writer.endElement("input");
-		
+
 		// decrement button
-		writer.startElement("input", this);
+		writer.startElement("input", component);
 		writer.writeAttribute("type", "submit", null);
 		writer.writeAttribute("name", clientId + ".less", null);
 		writer.writeAttribute("value", "<", "value");
 		writer.endElement("input");
-		
+
 		// increment button
-		writer.startElement("input", this);
+		writer.startElement("input", component);
 		writer.writeAttribute("type", "submit", null);
 		writer.writeAttribute("name", clientId + ".more", null);
 		writer.writeAttribute("value", ">", "value");
 		writer.endElement("input");
 	}
-	
+
 	@Override
-	public void decode(FacesContext fc) {
+	public void decode(FacesContext fc, UIComponent component) {
 		Map<String, String> requesParamtMap = fc.getExternalContext().getRequestParameterMap();
-		String clientId = getClientId(fc);
-		
+		String clientId = component.getClientId(fc);
+
 		int increment;
-		
+
 		if (requesParamtMap.containsKey(clientId + ".more")) {
 			increment = 1;
 		} else if (requesParamtMap.containsKey(clientId + ".less")) {
@@ -64,41 +63,55 @@ public class UISpinner extends UIInput {
 		} else {
 			increment = 0;
 		}
-		
+
 		try {
 			int submittedValue = Integer.parseInt(requesParamtMap.get(clientId));
-			int newValue = getIncrementedValue(submittedValue, increment);
-			setSubmittedValue("" + newValue);
+			int newValue = getIncrementedValue(submittedValue, increment, component);
+			((EditableValueHolder) component).setSubmittedValue("" + newValue);
 		} catch (NumberFormatException e) {
-			setSubmittedValue(requesParamtMap.get(clientId));
+			((EditableValueHolder) component).setSubmittedValue(requesParamtMap.get(clientId));
 		}
 	}
-	
-	private int getIncrementedValue(int submittedValue, int increment) {
-		Integer minimum = toInteger(getAttributes().get("minimum"));
-		Integer maximum = toInteger(getAttributes().get("maximum"));
+
+	@Override
+	public Object getConvertedValue(FacesContext fc, UIComponent component, Object submittedValue) throws ConverterException {
+		if (submittedValue instanceof String) {
+			Converter converter = ((ValueHolder) component).getConverter();
+
+			if (converter != null) {
+				return converter.getAsObject(fc, component, (String) submittedValue);
+			}
+		}
+
+		return submittedValue;
+	}
+
+	private static int getIncrementedValue(int submittedValue, int increment, UIComponent component) {
+		Integer minimum = toInteger(component.getAttributes().get("minimum"));
+		Integer maximum = toInteger(component.getAttributes().get("maximum"));
 		int newValue = submittedValue + increment;
-		
+
 		if ((minimum == null || newValue >= minimum) && (maximum == null || newValue <= maximum)) {
 			return newValue;
 		} else {
 			return submittedValue;
 		}
 	}
-	
+
 	private static Integer toInteger(Object value) {
 		if (value == null) {
 			return null;
 		}
-		
+
 		if (value instanceof Number) {
 			return ((Number) value).intValue();
 		}
-		
+
 		if (value instanceof String) {
 			return Integer.parseInt((String) value);
 		}
-		
+
 		throw new IllegalArgumentException("Cannot convert " + value);
 	}
+
 }
